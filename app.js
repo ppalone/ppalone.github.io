ca2737ee47b76bf710d4ff8dbd8a662c3b9b0b60
ppalone <pranjalalone@gmail.com>
2020-12-03 05:21:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+window.onload = function () {
+  const tableBody = document.querySelector('tbody');
+  let projects = [];
+
+  const getProjects = async () => {
+    try {
+      let response = await fetch(
+        'https://api.github.com/users/ppalone/repos'
+      );
+      if (response.status === 200) {
+        projects = await response.json();
+        setProjects(projects);
+      }
+    } catch (err) {
+      tableBody.innerHTML = 'Unable to fetch Projects'
+    }
+  }
+
+  const setProjects = (projects) => {
+    projects.forEach(project => {
+      let row = document.createElement('tr');
+      row.innerHTML = `
+        <td>
+          <a href=${project.html_url}>${project.name}</a>
+        </td>
+        <td>${project.description}</td>
+        <td>${project.stargazers_count}</td>
+      `
+      tableBody.appendChild(row)
+    })
+  }
+
+  getProjects();
+
+}
